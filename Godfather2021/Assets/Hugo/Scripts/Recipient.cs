5c3cb50bbc7280ad263fbc0649285efaa243f88a
Hugo <hugo.tuil@edu.devinci.fr>
2021-09-07 18:20:09 +0200

    Scriptable Object

---
+++ Godfather2021/Assets/Hugo/Scripts/Recipient.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recipient : MonoBehaviour
+{
+
+    public LevelManager levelManager;
+
+    public int recipientAliment1;
+    public int recipientAliment2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
