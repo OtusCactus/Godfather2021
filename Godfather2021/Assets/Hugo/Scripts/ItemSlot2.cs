be377d993f9b7d6b88984657c9177a7d3f066a40
Artyu <65313164+Artyu24@users.noreply.github.com>
2021-09-09 11:38:11 +0200

    [FIX] Knife and Plate can't go in the prepa slot

--- Godfather2021/Assets/Hugo/Scripts/ItemSlot2.cs
+++ Godfather2021/Assets/Hugo/Scripts/ItemSlot2.cs
@@ -12,7 +12,7 @@ public class ItemSlot2 : MonoBehaviour, IDropHandler
     {
         print("item dropped");
         //if an object has been droppped, put it in the right place
-        if (eventData.pointerDrag != null)
+        if (eventData.pointerDrag != null && !eventData.pointerDrag.GetComponent<Knife>() && !eventData.pointerDrag.GetComponent<CombinationResult>())
         {
             eventData.pointerDrag.GetComponent<RectTransform>().position = GetComponent<RectTransform>().position;
             eventData.pointerDrag.GetComponent<DragDrop>().previousPos = GetComponent<RectTransform>().position;
